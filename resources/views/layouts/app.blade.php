8b616d645e479aafacfdea3f9ab22dba9b588c37
David Lopez <dimor22@gmail.com>
2026-03-21 17:17:17 -0700

    Added livewire injections

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,6 +28,7 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @livewireStyles
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
@@ -118,5 +119,7 @@
                 });
             }
         </script>
+
+        @livewireScripts
     </body>
 </html>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -28,6 +28,7 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @livewireStyles
     </head>
     <body class="font-sans text-gray-900 dark:text-gray-100 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center px-4 pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
@@ -122,5 +123,7 @@
                 });
             @endif
         </script>
+
+        @livewireScripts
     </body>
 </html>
